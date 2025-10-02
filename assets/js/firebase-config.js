a1189721351dd8eddad399c72520debf6ba46b92
Xiotch <xiotchtech@gmail.com>
2025-10-02 19:52:57 +0530

    Super Admin Panel
    Super Admin Panel Added For Testing

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhIAwkU0YTIPqHCnZaS8q-yyDCP8CktL0",
+  authDomain: "tecxua-a22c9.firebaseapp.com",
+  projectId: "tecxua-a22c9",
+  storageBucket: "tecxua-a22c9.firebasestorage.app",
+  messagingSenderId: "242362635527",
+  appId: "1:242362635527:web:179a57cf5a43bf8d77e1a2",
+  measurementId: "G-VXJR3QW5J3"
+};
